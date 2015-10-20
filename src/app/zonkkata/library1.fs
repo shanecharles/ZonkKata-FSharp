55be746ab5f49e7aef11d0dcda0c9efc410ee8fa
Shane Charles <shanecharles@burningicesolutions.com>
2015-10-20 16:00:07 -0500

    Use FsCheck to test for a Royal Roll.

--- src/app/zonkkata/library1.fs
+++ src/app/zonkkata/library1.fs
@@ -2,6 +2,7 @@
 
 module Roll =
     let CalculatePoints d =
-        match d with 
+        let sorted = d |> List.sort
+        match sorted with 
         | [1; 2; 3; 4; 5; 6] -> 1500
         | _                  -> 0
--- src/tests/zonkkata.tests/library1.fs
+++ src/tests/zonkkata.tests/library1.fs
@@ -1,7 +1,35 @@
 ﻿namespace ZonkKata.Tests
 
+open System
 open Xunit
 open Swensen.Unquote
+open FsCheck
+open FsCheck.Xunit
+
+type RoyalRoll = 
+    static member Roll() =
+        let g = gen {
+            let rand = new Random()
+            let order = [1 .. 5]
+                        |> List.rev
+                        |> List.map (fun x -> rand.Next(x))
+
+            let rec reorder o (l : int list) =
+                match o with 
+                | [] -> l
+                | h :: o' ->
+                    let n = List.nth l h
+                    let l' = l |> List.filter (fun x -> x <> n)
+                    n :: (reorder o' l')
+
+            return [1 .. 6]
+                    |> reorder order
+        }
+        g |> Arb.fromGen
+
+type RoyalRollPropertyAttribute () =
+    inherit PropertyAttribute (
+        Arbitrary = [| typeof<RoyalRoll> |])
 
 module BigRoller =
     [<Fact>]
@@ -10,10 +38,10 @@ module BigRoller =
         let actual = [1; 1; 1; 1; 1; 1] |> ZonkKata.Roll.CalculatePoints
         test <@ expected <= actual @>
 
-    [<Fact>]
-    let ``A roll of 1 through 6 should return 1500 points.`` () =
+    [<RoyalRollProperty>]
+    let ``A roll of 1 through 6 should return 1500 points.`` (roll : int list) =
         let expected = 1500
-        let actual = [1 .. 6] |> ZonkKata.Roll.CalculatePoints
+        let actual = roll |> ZonkKata.Roll.CalculatePoints
         test <@ expected = actual @>
 
     [<Fact>]
